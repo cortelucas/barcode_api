7a20ba8a380d9ac4d4062a506e1c01ae04cba19d
Lucas S. Corte <corte1994.lc@gmail.com>
2024-02-15 17:13:58 -0300

    test: ensure call validator error

--- src/validators/tag_creator_validator_test.py
+++ src/validators/tag_creator_validator_test.py
@@ -1,3 +1,4 @@
+from src.erros.error_types.http_unprocessable_entity import HttpUnprocessableEntity
 from .tag_creator_validator import tag_creator_validator
 
 
@@ -12,3 +13,13 @@ def test_tag_creator_validator():
     tag_creator_validator(request)
 
     assert True
+
+
+def test_tag_creator_validator_unprocessable_entity():
+    request = MockRequest(json={"product_code": 123})
+
+    try:
+        tag_creator_validator(request)
+        assert False
+    except Exception as error:
+        assert isinstance(error, HttpUnprocessableEntity)
